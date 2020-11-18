634145d7fcae9a5884bb9a12157a7e6a8d6eac69
ShovelCode <vde435@gmail.com>
2020-11-17 23:20:11 -0600

    Update list-checker.py

--- list-checker.py
+++ list-checker.py
@@ -1,4 +1,5 @@
-list_of_wads = ['maghq','exec','greenwar','pl2','ultima2b','unalign','vanguard','valiant_zd09e','yetis_arena','zvox2','zvox6','zvox8']
+list_of_wads = ['maghq','exec','greenwar','pl2','ultima2b','unalign','vanguard',
+                'valiant_zd09e','yetis_arena','zvox2','zvox6','zvox8']
 list_of_iwads = ['doom','doom1','doom2','plutonia','tnt']
 
 candidate = 'av'
