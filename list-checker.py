92c7e68a3357f170ab1155691c0f06b4f1bc566f
ShovelCode <vde435@gmail.com>
2020-11-29 00:15:56 -0600

    python 64 path

--- list-checker.py
+++ list-checker.py
@@ -1,6 +1,7 @@
 list_of_wads = ['maghq','exec','greenwar','pl2','ultima2b','unalign','vanguard',
                 'valiant_zd09e','yetis_arena','zvox2','zvox6','zvox8',
                 'zdmegactf-1h_rc3',
+                'obaddon.wad',
                 'zdctf_bsides.wad']
 list_of_iwads = ['doom','doom1','doom2','plutonia','tnt','odib']
 pwads_for_tnt = ['tntr']
