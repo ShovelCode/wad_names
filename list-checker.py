f4be2fc1c26ec90f84d22ca5b36a60296316be1a
ShovelCode <vde435@gmail.com>
2020-12-01 17:06:49 -0600

    Update list-checker.py

--- list-checker.py
+++ list-checker.py
@@ -2,10 +2,11 @@ list_of_wads = ['maghq','exec','greenwar','pl2','ultima2b','unalign','vanguard',
                 'valiant_zd09e','yetis_arena','zvox2','zvox6','zvox8',
                 'zdmegactf-1h_rc3',
                 'obaddon.wad',
-                'zdctf_bsides.wad']
+                'zdctf_bsides.wad',
+                'r-green']
 list_of_iwads = ['doom','doom1','doom2','plutonia','tnt','odib']
 pwads_for_tnt = ['tntr']
 
-candidate = 'zdmegactf-1h_rc3' #what to check for
+candidate = 'r-green' #what to check for
 
 print(candidate in list_of_wads)
